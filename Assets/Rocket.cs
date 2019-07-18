a3de24bf2f2f7dc7501e06a7f8937cc99f55e7c4
Vashudev Dhama <dhamavashu1119@gmail.com>
2019-07-18 23:25:19 +0530

    Physics and Rigid body ( Adding thrust to the rocket)

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody; //to store and access the reference of rigid body
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>(); // Get me the component of type rigid body by any mean.
     }
 
     // Update is called once per frame
@@ -21,7 +22,8 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space)) // Thrusting can occur while rotating too
         {
-            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up); // Apply force relative to rigid body's cordinate system.
+            //Vector3.up stands for xyz cordinate system with focus on up i.e., y axis.
         }
         //Right or left rotation can't occur at same time
         if (Input.GetKey(KeyCode.A))

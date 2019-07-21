b269d49bba979988c7b3780aa4a037386ee73a4c
Vashudev Dhama <dhamavashu1119@gmail.com>
2019-07-21 18:08:35 +0530

    Adding multiple audio clips.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -8,6 +8,10 @@ public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 100f; // SerializedField allow to change in inspector but not by other scripts ( Public can be change by other scripts as well).
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine; // No need to have a default audio clip. And it'll be used to keep track of audio clip to be played.
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
     Rigidbody rigidBody; //to store and access the reference of rigid body
     AudioSource audioSource; //to store and access the reference of audio source
 
@@ -26,12 +30,13 @@ public class Rocket : MonoBehaviour
     {
         if(state == State.Alive) // control during scene switching
         {
-            Thrust();
-            Rotate();
+            RespondToThrustInput();
+            RespondToRotateInput();
         }
         
     }
 
+    // Called every time a collision occurs
     void OnCollisionEnter(Collision collision)
     {
         if(state != State.Alive) // control during scene switching
@@ -45,15 +50,30 @@ public class Rocket : MonoBehaviour
                 break;
             case "Finish":
                 //switch the scene
-                state = State.Transcending;
-                Invoke("LoadNextScreen", 1f); // LoadNextScreen method will invoke after 1 sec.
+                StartSuccessSequence();
                 break;
             default:
                 //kill the player
-                state = State.Dead;
-                Invoke("LoadFirstScreen", 1f);
+                StartDeathSequence();
                 break;
         }
+    }
+
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        Invoke("LoadNextScreen", 1f); // LoadNextScreen method will invoke after 1 sec.
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dead;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstScreen", 1f);
     }
 
     private void LoadFirstScreen()
@@ -66,16 +86,11 @@ public class Rocket : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
-    private void Thrust()
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.Space)) // Thrusting can occur while rotating too
         {
-            if (audioSource.isPlaying == false) // To avoid layering audio play for continuous space key pressing.
-            {
-                audioSource.Play();
-            }
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust); // Apply force relative to rigid body's cordinate system.
-                                                                    //Vector3.up stands for xyz cordinate system with focus on up i.e., y axis.
+            ApplyThrust();
         }
         else // To stop playing audio if space is not pressed.
         {
@@ -84,7 +99,17 @@ public class Rocket : MonoBehaviour
         
     }
 
-    private void Rotate()
+    private void ApplyThrust()
+    {
+        if (audioSource.isPlaying == false) // To avoid layering audio play for continuous space key pressing.
+        {
+            audioSource.PlayOneShot(mainEngine); // To play the clip posses by mainEngine.
+        }
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust); // Apply force relative to rigid body's cordinate system.
+                                                             //Vector3.up stands for xyz cordinate system with focus on up i.e., y axis.
+    }
+
+    private void RespondToRotateInput()
     {
         rigidBody.freezeRotation = true; // control rotation manually
 
